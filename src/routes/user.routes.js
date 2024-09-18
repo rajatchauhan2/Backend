d46fbdac285999f2f705ebb3fa35f9c0f3f617fe
rajatchauhan1 <rajat.tca1901089@tmu.ac.in>
2024-09-18 15:06:57 +0530

    Spelling mistake

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser } from "../conttrolers/user.controller.js";
 
 const router = Router()
 
